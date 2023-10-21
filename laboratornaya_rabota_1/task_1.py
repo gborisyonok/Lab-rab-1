220c3af927fe125297ce3bde2989a0c4e960d773
gborisyonok <148484570+gborisyonok@users.noreply.github.com>
2023-10-21 21:33:47 +0300

    laboratornaya_rabota_1
    Получилось выполнить все 4 задания, но есть вопрос в правильности понимания 4-го задания(сделала, как поняла).

---
+++ laboratornaya_rabota_1/task_1.py
@@ -0,0 +1,15 @@
+#целые числа
+a = 1
+print(a, type(a))
+#Числа с плавающей запятой
+b = 0.1
+print(b, type(b))
+
+#Логический тип данных
+a = 3
+b = 5
+print(bool(a < b), type(a < b))
+
+#Пустой тип данных
+c = None
+print(c, type(c))
---
+++ laboratornaya_rabota_1/task_2.py
@@ -0,0 +1,30 @@
+#Строки
+full_name = 'Gloria ' + 'Borisyonok' #сложение строк
+print(full_name)
+
+message = 'Ура!'
+repeated_message = message * 3
+print(repeated_message)
+
+#Списки
+concatenated_list = [3, 4, 5] + [7, 8, 9]
+print(concatenated_list)
+
+multiplied_list = [7, 9, 11] * 5
+print(multiplied_list)
+
+#Кортежи
+tuple_ = (5, 6, 7)
+print(tuple_)
+
+#Словари
+week ={
+    1:'понедельник',
+    2:'вторник',
+    3:'среда'
+}
+print('дни недели', week)
+
+#Множества
+set_ = {9, 'hello'}
+print(set_)
