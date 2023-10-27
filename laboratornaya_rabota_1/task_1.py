373be479e5561b17f440f7b73ca320a154c3ad79
gborisyonok <148484570+gborisyonok@users.noreply.github.com>
2023-10-27 19:20:54 +0300

    Update task_1.py

--- laboratornaya_rabota_1/task_1.py
+++ laboratornaya_rabota_1/task_1.py
@@ -1,15 +1,10 @@
-#целые числа
-a = 1
-print(a, type(a))
-#Числа с плавающей запятой
-b = 0.1
-print(b, type(b))
+numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 
-#Логический тип данных
-a = 3
-b = 5
-print(bool(a < b), type(a < b))
+# TODO заменить значение пропущенного элемента средним арифметическим
+missing_item = 4
+lenght_ = len(numbers)
+sum_numbers = sum(numbers[:missing_item]) + sum(numbers[missing_item+1:])
+sr_ar = sum_numbers / lenght_
 
-#Пустой тип данных
-c = None
-print(c, type(c))
+numbers[missing_item] = sr_ar
+print("Измененный список:", numbers)
